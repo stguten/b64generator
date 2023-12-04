602585aa5c33b1dc95748fe8101534976fb63978
StGuten <saddam.gutem@gmail.com>
2023-12-04 11:52:31 -0300

    update simples

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 import app from "./config/express.config.js";
-import b64 from "./router/encodeDecode.route.js";
 
-app.use('/',b64);
+app.post('/encode', encode);
+app.get('/decode', decode);
+
+app.get('/', (req, res) => {
+    res.status(200).send({
+        code: 200,
+        message: "B64 Linker Api versão 1.0.0",
+        timestamp: new Date(Date.now()).toLocaleString("pt-BR", { timeZone: "America/Fortaleza" }),
+    });
+});
+
+app.use((req, res) => {
+    res.status(404).send({
+        code: 404,
+        message: "Endpoint não encontrado :/",
+        timestamp: new Date(Date.now()).toLocaleString("pt-BR", { timeZone: "America/Fortaleza" }),
+    });
+});
 
 export default app;
--- src/config/express.config.js
+++ src/config/express.config.js
@@ -1,11 +1,9 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import compression from "compression";
 
 const app = express();
 
-app.use(compression());
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
